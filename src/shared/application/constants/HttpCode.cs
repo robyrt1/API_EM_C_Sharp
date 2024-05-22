5e09f4f2f8efa3fe4d03921c5ec4893e459417b0
robyrt1 <robertpinto1190.rpm@gmail.com>
2024-05-22 10:59:53 -0300

    criando enum de HttpCode

---
+++ src/shared/application/constants/HttpCode.cs
@@ -0,0 +1,20 @@
+﻿namespace API_EM_C_.src.shared.application.constants
+{
+    public enum HttpStatusCode
+    {
+        OK = 200,
+        CREATED = 201,
+        NO_CONTENT = 204,
+        NOT_MODIFIED = 304,
+        BAD_REQUEST = 400,
+        UNAUTHORIZED = 401,
+        NOT_PAYMENT_REQUIRED = 402,
+        FORBIDDEN = 403,
+        NOT_FOUND = 404,
+        NOT_ACCEPTABLE = 406,
+        REQUEST_TIMEOUT = 408,
+        CONFLICT = 409,
+        INTERNAL_SERVER_ERROR = 500,
+        NOT_IMPLEMENTED = 501,
+    }
+}

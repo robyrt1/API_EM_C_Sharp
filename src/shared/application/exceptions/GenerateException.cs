333c904a514c641bf5c2855cb7fb46d730e7b3e4
robyrt1 <robertpinto1190.rpm@gmail.com>
2024-05-22 11:00:52 -0300

    feat: add class de gerar um exception sempre padrao

---
+++ src/shared/application/exceptions/GenerateException.cs
@@ -0,0 +1,21 @@
+﻿using API_EM_C_.src.shared.application.exceptions.type;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace API_EM_C_.src.shared.application.exceptions
+{
+    public class GenerateException : IGenerateException
+    {
+        public Array Data { get; set; }
+        public string Message { get; set; }
+        public int StatusCode { get; set; }
+        public bool Status { get; } = false;
+
+        public GenerateException(Array data, string message, int statusCode, bool status = false)
+        {
+            Data = data;
+            Message = message;
+            StatusCode = statusCode;
+            Status = status;
+        }
+    }
+}

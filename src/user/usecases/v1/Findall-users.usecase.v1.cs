dc7a4c38744845d1ba170194742e3da52d4f855b
robyrt1 <robertpinto1190.rpm@gmail.com>
2024-05-24 20:36:31 -0300

    passando o interface no use case

--- src/user/usecases/v1/Findall-users.usecase.v1.cs
+++ src/user/usecases/v1/Findall-users.usecase.v1.cs
@@ -1,6 +1,7 @@
 ﻿using API_EM_C_.src.shared.application.constants;
 using API_EM_C_.src.shared.application.exceptions;
 using API_EM_C_.src.user.domain.entity.v1;
+using API_EM_C_.src.user.domain.port.repository;
 using API_EM_C_.src.user.domain.port.usecases.v1;
 using API_EM_C_.src.user.infra.repository;
 
@@ -8,7 +9,7 @@ namespace API_EM_C_.src.user.usecases.v1
 {
     public class FindallUsersUsecase: IFindAllUsers
     {
-        private readonly UserRepository _userRepository;
+        private readonly IRepositoryUser _userRepository;
 
         public FindallUsersUsecase(UserRepository userRepository)
         {
